d55ecda5c662cfa054b172a6e7f4bcacf0fba861
ManpreetKaur2023 <saini_preet135@yahoo.com>
2023-07-08 22:18:37 -0400

    updated pom

--- src/main/java/com/naveenautomationlabs/automationFramework/Listeners/CustomListeners.java
+++ src/main/java/com/naveenautomationlabs/automationFramework/Listeners/CustomListeners.java
@@ -1,5 +1,6 @@
 package com.naveenautomationlabs.automationFramework.Listeners;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -25,5 +26,20 @@ public class CustomListeners extends TestBase  implements ITestListener {
 	logger.info("Test  "+result.getMethod().getMethodName() + " Skipped "+" ###############################");
 		
 	}
+@Override
+public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void onStart(ITestContext context) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void onFinish(ITestContext context) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
